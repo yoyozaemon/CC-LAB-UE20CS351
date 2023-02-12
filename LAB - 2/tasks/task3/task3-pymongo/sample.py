6d4c4183ebc956517ea9545ec8bc21f32edd20f8
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2023-02-12 08:11:55 +0530

    Lab - 2 task code added

---
+++ LAB - 2/tasks/task3/task3-pymongo/sample.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+host = MongoClient("172.17.0.2")
+#host = MongoClient("mongodb")
+
+db = host["sample_db"]
+collection = db["sample_collection"]
+
+sample_data = {"Name:":"Vijay J","SRN":"PES2UG20CS815"}
+collection.insert_one(sample_data)
+print('Inserted into the MongoDB database!')
+
+rec_data = collection.find_one({"SRN":"PES2UG20CS815"})
+print("Fecthed from MongoDB: ",rec_data)
